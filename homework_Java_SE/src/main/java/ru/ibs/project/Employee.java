92dd18151d9f7ca3e7f2a89d5479575be37ed16e
Di-Lavanda <50248066+Di-Lavanda@users.noreply.github.com>
2021-10-24 18:16:16 +0400

    Add get & set methods for Employee class
    Добавил геттер и сеттер для класса Сотрудник

--- homework_Java_SE/src/main/java/ru/ibs/project/Employee.java
+++ homework_Java_SE/src/main/java/ru/ibs/project/Employee.java
@@ -4,4 +4,20 @@ public abstract class Employee {
     protected int workTime;
     protected int rate;
     public abstract int salary();
+
+    public int getWorkTime() {
+        return workTime;
+    }
+
+    public void setWorkTime(int workTime) {
+        this.workTime = workTime;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
 }
